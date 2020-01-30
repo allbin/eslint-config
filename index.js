f8b5d889960b9ad54faf07424d21a881cb1fb363
Mathias Djärv <mathias.djarv@gmail.com>
2020-01-30 15:58:10 +0100

    set explicit-function-return-type.allowExpressions to true

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    project: ["tsconfig.json"]
+  },
+  plugins: ["prettier", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true
+      }
     ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        project: ["tsconfig.json"]
-    },
-    plugins: ['prettier', '@typescript-eslint'],
-    rules: {
-        'prettier/prettier': 'error',
-        '@typescript-eslint/camelcase': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        camelcase: 'off'
-    },
+    camelcase: "off"
+  }
 };
